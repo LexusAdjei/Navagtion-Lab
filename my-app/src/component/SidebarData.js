1b34ce5df04e8bdaa8f0b36ef7c538772e3d4039
Lexus Adjei <lexus.adjei@cdpipelinedevshop.com>
2022-07-13 16:57:56 +0000

    first commit

---
+++ my-app/src/component/SidebarData.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import HomeIcon from '@mui/icons-material/Home';
+import MailIcon from '@mui/icons-material/Mail';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import GroupIcon from '@mui/icons-material/Group';
+import ImageSearchIcon from '@mui/icons-material/ImageSearch';
+
+export const SidebarData = [
+    {
+    title: "Home",
+    icon: <HomeIcon />,
+    link: "/home" 
+},
+{
+    title: "Mail",
+    icon: <MailIcon />,
+    link: "/mail"
+},
+{
+    title: "Analytics",
+    icon: <AnalyticsIcon />,
+    link: "/analyticsicon"
+},
+{
+    title: "DashboardIcon",
+    icon: <DashboardIcon />,
+    link: "/DashboardIcon"
+},
+{
+    title: "GroupIcon",
+    icon: <GroupIcon />,
+    link: "/groupicon",
+
+    title: "ImageSearchIcon",
+    icon: <ImageSearchIcon />,
+    link: "/imagesearchicon",
+
+
+
+}
+   
+
+
+
+]
